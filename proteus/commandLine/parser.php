c513709f8007adeba9dc925487508d8695f75e8f
Paul McNab <mac.paul.1965@gmail.com>
2018-02-26 16:48:55 +1000

    Got distracted today
    Lost my thought track

---
+++ proteus/commandLine/parser.php
@@ -0,0 +1,118 @@
+<?php
+
+CommandLineParser($argc, $argv);
+
+
+/**
+ *
+ */
+function CommandLineParser($argc, $argv)
+{
+	assert(is_numeric($argc), "ARGC should be an integer. Maybe parameter order is mixed up");
+	assert(is_array($argv), "ARGV should be an array. Maybe parameter order is mixed up");
+	
+	for ($i = 0; $i < $argc; $i++)
+	{
+		$val = $argv[$i];
+		
+		
+        switch($val[0])
+		{
+			case "/":
+				//printf("Val %s\n", $val);
+				CommandValidate($val, $i, $argc);
+				break;
+				
+			default:
+				//printf("Val %s\n", $val);
+				break;
+		}
+	}
+}
+
+
+function CommandValidate($command, $index, $count)
+{
+	//$logLevels = array("verbose", "debug", "info", "warn", "error");
+	
+	// Place outside loop - stop constant creation.
+	$commands = array(array("/LogLevel", 1, "LogLevelValidator", array("verbose", "debug", "info", "warn", "error")),
+					  array("/LogFile", 1, "LogFileValidator", NULL),
+					  array("/Help", 0, NULL, NULL)
+					 );
+					  
+  	$cmd_count = count($commands);
+
+	$found = FALSE;
+	
+	for ($i=0; $i < $cmd_count; $i++)
+	{
+		if (in_array($command, $commands[$i]))
+		{
+			$found = TRUE;
+			
+			$parameters = $commands[$i][1];		// Get parameter count.
+			$validator  = $commands[$i][2];		// Get parameter validator.
+			$switches   = $commands[$i][3];
+			
+			//print($parameters);
+			//var_dump($validator);			
+/*			if ($switches != NULL)
+			{
+				var_dump($switches);
+			}//*/
+			echo "Command $command\n";
+			
+			if ($parameters > 0)
+			{
+				GetSwitchParameters($i, $cmd_count, $switches);
+				
+				if ($validator != NULL)
+				{
+					$validator(NULL, $switches);
+				}
+			}
+			break;
+		}
+	}
+	
+	
+//	if ($found)
+//	{
+	//	echo "Command $command at $index is found\n";
+	//}
+}
+
+
+function GetSwitchParameters($index, $count, $params)
+{	
+	echo "Index $index of $count\n";
+	
+	if ($index < $count)
+	{
+		echo "Params $params\n";
+
+		if ($params > 0)
+		{
+			
+		}
+	}
+}
+
+
+function LogLevelValidator($setting, $parameters)
+{
+	//echo "Called LogLevelValidator\n";	
+	//echo $parameters;
+	if (is_array($parameters))
+	{		
+		//echo $parameters[0];
+	}
+}
+
+
+function LogFileValidator($setting, $parameters)
+{
+	//echo "Called LogFileValidator\n";
+	//echo $parameters;
+}
--- proteus/errors/Exceptions.php
+++ proteus/errors/Exceptions.php
@@ -1,11 +1,43 @@
 <?php
 // Investigate this further.
-class customException extends Exception
+class NonNumericException extends Exception
 {
+    private $value;
+    
+    
+    function __construct($value)
+    {
+        $this->value = $value;
+    }
+    
+    
+    public function info() 
+    {
+        //printf($this->msg, $this->value);
+        $errorMsg = 'Error on line ' . $this->getLine() . ' in ' . $this->getFile()
+                    . ': <b>' . $this->getMessage() . '</b> is not a valid E-Mail address';
+        
+        return $errorMsg;
+    }
+    
   /*public function errorMessage() {
     //error message
     $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile()
     .': <b>'.$this->getMessage().'</b> is not a valid E-Mail address';
     return $errorMsg;
   }//*/
+}
+
+
+
+class NotAString
+{
+    private $value;
+    
+    
+    function __construct($value)
+    {
+        $this->value = $value;
+    }
+    
 }
